f384c7da00e3b2e8eb3bd3c9d5bde9bbd693350a
gycsaba <gyuris.csaba@vasvari.org>
2022-02-23 22:01:18 +0100

    A feladat

---
+++ tests2/Tests2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+using CircleProject;
+using CircleProject.Models;
+
+namespace CircleProject.Tests
+{
+    [TestClass()]
+    public class Tests2
+    {
+        [TestMethod()]
+        public void TestCircle()
+        {
+            Circle c1 = new Circle(2);
+            Assert.ThrowsException<ExceptionCircleRadiusNegativOrZero>(() => c1.Radius=-2 );
+
+            try
+            {
+                Circle c2 = new Circle(-2);
+            }
+            catch (ExceptionCircleRadiusNegativOrZero)
+            {
+                return;
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Circle konstruktora nem a megfelelő kivételt dobja!\n"+e.Message);
+            }
+            Assert.Fail("A Circle konstruktora negatív sugár esetén nem dob kivételt!");
+
+
+            try
+            {
+                Circle c2 = new Circle(0);
+            }
+            catch (ExceptionCircleRadiusNegativOrZero)
+            {
+                return;
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Circle konstruktora nem a megfelelő kivételt dobja!\n" + e.Message);
+            }
+            Assert.Fail("A Circle konstruktora nulla sugarú kör esetén nem dob kivételt!");
+        }
+    }
+}
