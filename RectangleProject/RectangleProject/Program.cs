a731a94b6b289ce29716eb12084eff74599d56a0
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-24 09:03:23 +0100

    Feladat és tesztek

---
+++ RectangleProject/RectangleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RectangleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
